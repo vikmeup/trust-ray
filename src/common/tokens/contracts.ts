087b87b951c36d94148ff48bc62c013ee1c29dab
Viktor Radchenko <yazexel@gmail.com>
2018-03-12 22:07:40 -0700

    Start mapping smart contract to coinmarket id

--- src/common/tokens/contracts.ts
+++ src/common/tokens/contracts.ts
@@ -1,143 +1,83 @@
 module.exports = {
     "0x1d462414fe14cf489c7a21cac78509f4bf8cd7c0": {
-        id: "canyacoin",
-        symbol: "CAN"
+        id: "canyacoin"
     },
     "0x5f3789907b35dce5605b00c0be0a7ecdbfa8a841": {
         id: "content-and-ad-network",
-        symbol: "CAN"
     },
     "0xdd974d5c2e2928dea5f71b9825b8b646686bd200": {
-        id: "kyber-network",
-        symbol: "KNC"
+        id: "kyber-network"
     },
-    "0x1234567461d3f8db7496581774bd869c83d51c93": {
-        id: "bitclave",
-        symbol: "CAT"
+    "0x1234567461d3f8db749681774bd869c83d51c93": {
+        id: "bitclave"
     },
     "0x13f1b7fdfbe1fc66676d56483e21b1ecb40b58e2": {
-        id: "accelerator-network",
-        symbol: "ACC"
+        id: "accelerator-network"
     },
     "0x8aa33a7899fcc8ea5fbe6a608a109c3893a1b8b2": {
-        id: "dao-casino",
-        symbol: "BET"
+        id: "dao-casino"
     },
     "0xfae4ee59cdd86e3be9e8b90b53aa866327d7c090": {
-        id: "cpchain",
-        symbol: "CPC"
+        id: "cpchain"
     },
     "0xe0c72452740414d861606a44ccd5ea7f96488278": {
-        id: "encryptotel-eth",
-        symbol: "ETH"
+        id: "encryptotel-eth"
     },
     "0x2ae965cd3d2b6d186e87d9586fc3bdbfc667cacc": {
-        id: "global-jobcoin",
-        symbol: "GJC"
+        id: "global-jobcoin"
     },
     "0xb70835d7822ebb9426b56543e391846c107bd32c": {
-        id: "game",
-        symbol: "GTC"
+        id: "game"
     },
     "0x888666ca69e0f178ded6d75b5726cee99a87d698": {
-        id: "iconomi",
-        symbol: "ICN"
+        id: "iconomi"
     },
     "0x647f274b3a7248d6cf51b35f08e7e7fd6edfb271": {
-        id: "maggie",
-        symbol: "MAG"
+        id: "maggie"
     },
     "0x449574c69f3a658794829ed81639a7a9ece041e1": {
-        id: "neo-gold",
-        symbol: "NEOG"
+        id: "neo-gold"
     },
     "0xcfb98637bcae43c13323eaa1731ced2b716962fd": {
-        id: "nimiq",
-        symbol: "NET"
+        id: "nimiq"
     },
     "09800417b097c61b9fd26b3ddde4238304a110d5": {
-        id: "qbao",
-        symbol: "QBT"
+        id: "qbao"
     },
     "0x7dc4f41294697a7903c4027f6ac528c5d14cd7eb": {
-        id: "remicoin",
-        symbol: "RMC"
+        id: "remicoin"
     },
     "0x6f6deb5db0c4994a8283a01d6cfeeb27fc3bbe9c": {
-        id: "smartbillions",
-        symbol: "SMART"
+        id: "smartbillions"
     },
     "0x02eb3da777eb3ff40914fd3d5c249123cc2df04a": {
-        id: "edu-stars-coin",
-        name: "Edu Stars Coin",
-        symbol: "EDSTR"
+        id: "edu-stars-coin"
     },
     "0xa7adc14fc624617a18e5a476d80a7fefe90a11b7": {
-        id: "edustar-lite",
-        name: "Edustar Lite",
-        symbol: "EDSLTE"
-    },
-    "0xddcaf6c604592b37f775f3d303a02013bbd7ad93": {
-        id: "",
-        name: "CWC",
-        symbol: "CWC"
-    },
-    "0x4c54a332e3c962d5e513c3cc927c4ccf3452c79c": {
-        id: "",
-        name: "UET",
-        symbol: "UET"
-    },
-    "0xc8110527a0b803c0ba15456861d720864915b6ed": {
-        id: "",
-        name: "DET",
-        symbol: "DET"
+        id: "edustar-lite"
     },
     "0x2c4e8f2d746113d0696ce89b35f0d8bf88e0aeca": {
-        id: "simple-token",
-        symbol: "ST"
-    },
-    "0x75c79b88face8892e7043797570c390bc2db52a7": {
-        id: "",
-        name: "Gas Files",
-        symbol: "GAS"
+        id: "simple-token"
     },
     "0x5e4abe6419650ca839ce5bb7db422b881a6064bb": {
-        id: "wi-coin",
-        name: "Wi Coin",
-        symbol: "WIC"
+        id: "wi-coin"
     },
     "0x629aee55ed49581c33ab27f9403f7992a289ffd5": {
-        name: "StrikeCoin Token",
-        symbol: "STC"
+        name: "StrikeCoin Token"
     },
     "0xf41e5fbc2f6aac200dd8619e121ce1f05d150077": {
-        name: "CrycashToken",
-        symbol: "SRC"
+        name: "CrycashToken"
     },
     "0x42901532700341c0b26854fa7c0eda1a66f1158e": {
-        name: "Geo Coins",
-        symbol: "GEO"
+        name: "Geo Coins"
     },
     "0x45555629aabfea138ead1c1e5f2ac3cce2add830": {
-        name: "CandyHCoin",
-        symbol: "CANDY"
+        name: "CandyHCoin"
     },
     "0x78b039921e84e726eb72e7b1212bb35504c645ca": {
-        id: "sether",
-        symbol: "SETH",
-        name: "Sether"
+        id: "sether"
     },
     "0x226bb599a12c826476e3a771454697ea52e9e220": {
-        id: "propy",
-        symbol: "PRO",
-        name: "Propy"
+        id: "propy"
     },
-    "0x0235fe624e044a05eed7a43e16e3083bc8a4287a": {
-        name: "Original Crypto Coin",
-        symbol: "OCC"
-    },
-    "0x92413fd4370c2112284bf27cee993d6631fec4d4": {
-        name: "Original Crypto Coin",
-        symbol: "OCC"
-    }
 }
